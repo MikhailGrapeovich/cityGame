56dfcaf9f0f5c8c7ba307ba53c0f752d38080951
Михаил <vinogradov.m.y@gmail.com>
2024-05-31 14:43:23 +0300

    исправление ошибок

--- dataBase.py
+++ dataBase.py
@@ -1,40 +1,52 @@
 import sqlite3
 
-con = sqlite3.connect("cities_db.db")
-cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS users(chat_id INT PRIMARY KEY, score INT, cities TEXT)")
-con.commit()
-con.close()
+
 def connection_db(func):
     def wrap(*args, **kwargs):
         try:
             con = sqlite3.connect("cities_db.db")
             cur = con.cursor()
-            r = func(*args,**kwargs, cur=cur)
+            r = func(*args, **kwargs, cur=cur)
             con.commit()
             con.close()
             return r
         except sqlite3.Error as e:
             con.close()
             print(f"ошибка базы данных - {e}")
+
     return wrap
+
+
 @connection_db
-def add_db(chat_id,score,cities, cur):
+def start():
+    cur.execute("CREATE TABLE IF NOT EXISTS users(chat_id INT PRIMARY KEY, score INT, cities TEXT)")
+
+
+@connection_db
+def add_db(chat_id, score, cities, cur):
     cur.execute(f"INSERT INTO users(chat_id, score, cities)  VALUES({chat_id}, {score}, '{cities}')")
+
+
 @connection_db
 def get_all_db(cur):
     cur.execute(f"SELECT * FROM users")
     return cur.fetchall()
 
+
 @connection_db
 def get_db(chat_id, cur):
     cur.execute(f"SELECT * FROM users WHERE chat_id={chat_id}")
     return cur.fetchone()
+
+
 @connection_db
-def update_db(chat_id,score,cities, cur):
+def update_db(chat_id, score, cities, cur):
     cur.execute(f"UPDATE users SET score={score}, cities='{cities}' WHERE chat_id={chat_id}")
+
 
 @connection_db
 def delete(chat_id, cur):
     cur.execute(f"DELETE FROM users WHERE chat_id = {chat_id}")
-#delete(1269131474)
+
+
+start()
